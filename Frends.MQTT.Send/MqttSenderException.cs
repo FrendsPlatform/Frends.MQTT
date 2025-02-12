7d6acfec86f4fe7fda88c96458694619905d32d4
JakubSzubert <jakub.szubert@frends.com>
2025-02-12 11:22:10 +0100

    Delete Frends.MQTT.Send directory

--- Frends.MQTT.Send/MqttSenderException.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public class MqttSenderException : Exception
-{
-    public MqttSenderException(string message, Exception innerException)
-        : base(message, innerException) { }
-}
